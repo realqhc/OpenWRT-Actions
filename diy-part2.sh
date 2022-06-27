20a073730a901bf8c32b9b93f91b85809bc3ea4a
realqhc <98016705+realqhc@users.noreply.github.com>
2022-06-27 14:54:24 +1000

    Initial commit

---
+++ diy-part2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#============================================================
+# https://github.com/Lancenas/Actions-Lean-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+
+# Modify default IP
